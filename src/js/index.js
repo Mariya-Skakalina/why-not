cdb0944608f7cc787cbf0bacf698c36f58a79cdd
Mariya-Skakalina <scka.maria@yandex.ru>
2020-11-14 04:46:04 +0400

    Редактирование стилей

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,16 @@
 console.log('Hi')
 import '../style/style.scss'
+
+let yes_modalka = document.querySelector('#yes')
+let no_modalka = document.querySelector('#no')
+let modalka = document.querySelector('.modal-wrapper')
+
+if(getComputedStyle(modalka).display == "flex") {
+    yes_modalka.addEventListener('click', () => {
+        modalka.style.display = "none"
+        document.body.style.overflowY = "auto"
+    })
+    no_modalka.addEventListener('click', () => {
+        window.close()
+    })
+}
